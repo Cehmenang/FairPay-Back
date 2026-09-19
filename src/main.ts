947312e304fcd61208b1753ef555032ad203265f
Cehmenang <heaven.cehwin12@gmail.com>
2026-09-19 09:40:04 +0700

    Menambahkan Payment Method2

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
-  app.enableCors({ origin: 'http://localhost:5173', credentials: true })
+  app.enableCors({ origin: 'https://fairpay.cehwin.cloud', credentials: true })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
