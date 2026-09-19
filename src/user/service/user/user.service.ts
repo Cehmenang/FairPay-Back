d7e8f9a69e72fb95785430cfb295abe3dc3f9db8
Cehmenang <heaven.cehwin12@gmail.com>
2026-09-19 12:33:53 +0700

    Menambahkan Payment Method7

--- src/user/service/user/user.service.ts
+++ src/user/service/user/user.service.ts
@@ -10,7 +10,7 @@ export class UserService {
 
     generateAccessToken(token: string, response: Response){
         return response.cookie('accessToken', token, {
-            maxAge: 1000 * 60 * 60, httpOnly: true, secure: true, sameSite: 'none'
+            maxAge: 1000 * 60 * 60, httpOnly: true, secure: true, sameSite: 'none' as const
         })
     }
 
